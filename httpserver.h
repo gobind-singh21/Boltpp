18d7e8993d75c987010cd72e10ddb4704e5bbec7
Gobind Singh Maan <sgobind577@gmail.com>
2025-02-28 02:23:13 +0530

    modified:   .gitignore 	modified:   httpserver.h 	modified:   request.h

--- httpserver.h
+++ httpserver.h
@@ -33,23 +33,96 @@ class HttpServer {
 
   std::unordered_map<std::string, Route> allowed;
 
+  // void sendResponse(const SOCKET clientSocket, const std::string response) {
+
+  // }
+
   // Private static helper functions.
   static std::string getStatusCodeWord(const int statusCode) {
     switch(statusCode) {
+      // 1xx Informational
+      case 100: return "Continue";
+      case 101: return "Switching Protocols";
+      case 102: return "Processing";
+      case 103: return "Early Hints";
+
+      // 2xx Successful
       case 200: return "OK";
       case 201: return "Created";
       case 202: return "Accepted";
+      case 203: return "Non-Authoritative Information";
+      case 204: return "No Content";
+      case 205: return "Reset Content";
+      case 206: return "Partial Content";
+      case 207: return "Multi-Status";
+      case 208: return "Already Reported";
+      case 226: return "IM Used";
+
+      // 3xx Redirection
+      case 300: return "Multiple Choices";
+      case 301: return "Moved Permanently";
+      case 302: return "Found";
+      case 303: return "See Other";
+      case 304: return "Not Modified";
+      case 305: return "Use Proxy";
+      case 306: return "(Unused)";
+      case 307: return "Temporary Redirect";
+      case 308: return "Permanent Redirect";
+
+      // 4xx Client Error
+      case 400: return "Bad Request";
+      case 401: return "Unauthorized";
+      case 402: return "Payment Required";
+      case 403: return "Forbidden";
+      case 404: return "Not Found";
+      case 405: return "Method Not Allowed";
+      case 406: return "Not Acceptable";
+      case 407: return "Proxy Authentication Required";
+      case 408: return "Request Timeout";
+      case 409: return "Conflict";
+      case 410: return "Gone";
+      case 411: return "Length Required";
+      case 412: return "Precondition Failed";
+      case 413: return "Payload Too Large";
+      case 414: return "URI Too Long";
+      case 415: return "Unsupported Media Type";
+      case 416: return "Range Not Satisfiable";
+      case 417: return "Expectation Failed";
+      case 421: return "Misdirected Request";
+      case 422: return "Unprocessable Entity";
+      case 423: return "Locked";
+      case 424: return "Failed Dependency";
+      case 425: return "Too Early";
+      case 426: return "Upgrade Required";
+      case 428: return "Precondition Required";
+      case 429: return "Too Many Requests";
+      case 431: return "Request Header Fields Too Large";
+      case 451: return "Unavailable For Legal Reasons";
+
+      // 5xx Server Error
+      case 500: return "Internal Server Error";
+      case 501: return "Not Implemented";
+      case 502: return "Bad Gateway";
+      case 503: return "Service Unavailable";
+      case 504: return "Gateway Timeout";
+      case 505: return "HTTP Version Not Supported";
+      case 506: return "Variant Also Negotiates";
+      case 507: return "Insufficient Storage";
+      case 508: return "Loop Detected";
+      case 510: return "Not Extended";
+      case 511: return "Network Authentication Required";
     }
     return "Not Found";
   }
 
+
   static std::string makeHttpResponse(const Res &res) {
     int statusCode = res.getStatusCode();
     std::string response = res.getProtocol() + " " + std::to_string(statusCode) + " " +
-                           getStatusCodeWord(statusCode) + "\r\n";
+                           getStatusCodeWord(statusCode) + "\n\r";
     for(const auto &it : res.headers)
-      response += it.first + ": " + it.second + "\r\n";
-    response += "\r\n" + res.getPayload() + "\r\n";
+      response += it.first + ": " + it.second + "\n\r";
+    response += "\n\r" + res.getPayload() + "\n\r";
     return response;
   }
 
@@ -91,29 +164,71 @@ class HttpServer {
     int queryParamPos = path.find("?");
     if(queryParamPos != std::string::npos)
       queryParameters = getQueryParameters(path.substr(queryParamPos, path.length() - queryParamPos - 1));
+    bool headerEnd = false;
+    std::string payload = "";
 
     while(getline(ss, line)) {
-      if(line == "\r\n" || line == "\n")
-        break;
+      if(headerEnd) {
+        if(line.back() == '\r')
+          line.pop_back();
+        payload += line;
+        continue;
+      }
+      if(line == "\r" || line == "\n" || line == "\n\r")
+        headerEnd = true;
       std::pair<std::string, std::string> headerKeyValue = getHeaderKeyValue(line);
       headers.insert(headerKeyValue);
     }
-    return Req(method, path, protocol, queryParameters, headers);
+    return Req(method, path, protocol, payload, queryParameters, headers);
+  }
+
+  static void sendErrorResponse(Res &res, const SOCKET clientSocket, int statusCode) {
+    std::string errorMessage = getStatusCodeWord(statusCode);
+    JSONValue::Object message;
+    message["message"] = errorMessage;
+    JSONValue jsonMessage(message);
+    res.json(jsonMessage)->status(statusCode);
+    std::string responseMessage = makeHttpResponse(res);
+    send(clientSocket, responseMessage.c_str(), responseMessage.length(), 0);
   }
 
   // Private member functions to handle client connections.
   void handleClientRequest(const SOCKET clientSocket) {
-    char recvBuffer[1024];
+    char recvBuffer[10240];
     int bytesRecieved = recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
     
     if(bytesRecieved > 0) {
       recvBuffer[bytesRecieved] = '\0';
+      std::cout << recvBuffer << std::endl;
 
       std::string request(recvBuffer);
       
       Req req = parseHttpRequest(request);
       Res res;
       res.setProtocol(req.protocol);
+
+      if(req.headers.find("Content-Length") != req.headers.end()) {
+        int contentLength = -1;
+        if(req.headers.find("Content-Length") == req.headers.end()) {
+          sendErrorResponse(res, clientSocket, 400);
+          return;
+        }
+        contentLength = std::stoi(req.headers["Content-Length"]);
+        std::string payload = "";
+        int totalRecieved = request.size() - request.find("\r\n\r\n") - 4;
+        if(totalRecieved < contentLength) {
+          payload.append(request.substr(request.find("\r\n\r\n") + 4));
+          while(totalRecieved < contentLength) {
+            int remaining = std::min(4096, contentLength - totalRecieved);
+            int recieved = recv(clientSocket, recvBuffer, remaining, 0);
+            if(recieved <= 0)
+              break;
+            payload.append(recvBuffer, recieved);
+            totalRecieved += recieved;
+          }
+        }
+        req.payload = payload;
+      }
 
       std::string key = req.method + "::" + req.path;
       
--- request.h
+++ request.h
@@ -5,19 +5,20 @@
 
 class Req {
 public:
-  Req() : method(""), path(""), queryParameters({}), headers({}) {}
-  Req(std::string _method, std::string _path, std::string _protocol,
+  Req() : method(""), path(""), protocol("HTTP/1.1"), payload(""), queryParameters({}), headers({}) {}
+  Req(std::string _method, std::string _path, std::string _protocol, std::string _payload,
       std::unordered_map<std::string, std::string> query,
       std::unordered_map<std::string, std::string> _headers)
-      : method(_method), path(_path), protocol(_protocol),
+      : method(_method), path(_path), protocol(_protocol), payload(_payload),
         queryParameters(query), headers(_headers) {}
   Req(const Req &req)
-      : method(req.method), path(req.path), protocol(req.protocol),
+      : method(req.method), path(req.path), protocol(req.protocol), payload(req.payload),
         queryParameters(req.queryParameters), headers(req.headers) {}
 
   std::string method;
   std::string path;
   std::string protocol = "HTTP/1.1";
+  std::string payload;
   std::unordered_map<std::string, std::string> queryParameters;
   std::unordered_map<std::string, std::string> headers;
 };
