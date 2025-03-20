eb579a6f65b2669d2a359f0f68e38904f987e000
Gobind Singh Maan <sgobind577@gmail.com>
2025-03-21 01:24:01 +0530

    modified:   include/middlewares.h

--- include/middlewares.h
+++ include/middlewares.h
@@ -4,13 +4,9 @@
 #include <iostream>
 
 inline auto JsonBodyParser = [](Req &req, Res &res, long long &next) {
-  std::cout << req.headers["Content-Type"] << std::endl;
   if(req.headers["Content-Type"].find("application/json") != std::string::npos) {
-    std::cout << "Enter middleware control" << std::endl;
     try {
-      JSONValue json = JSONParser(req.payload).parse();
-      std::cout << json.stringify() << std::endl;
-      req.body = json;
+      req.body = JSONParser(req.payload).parse();
     } catch (const std::exception &e) {
       res.status(400)->send("Bad Request");
       next = -1;
