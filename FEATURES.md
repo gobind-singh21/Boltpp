6ad6bb519b3709ea477da52a8d5f76e787518abf
Gobind Singh Maan <sgobind577@gmail.com>
2025-05-24 21:31:31 +0530

    removed async (worse performance than original thread pool), added features.md to track feature completion

---
+++ FEATURES.md
@@ -0,0 +1,33 @@
+# Features
+
+- ## Http request parsing (completed)
+
+- ## Http connection establishing (completed)
+
+- ## Sending Http response (completed)
+
+- ## Request class (completed)
+
+- ## Response class (completed)
+
+- ## Middlewares (global and route specific) (completed)
+
+- ## Route handler (completed)
+
+- ## JSON class and parsing (completed)
+
+- ## Path parameters & Query parameters (Completed)
+
+- ## Server listen thread for establishing connection (completed)
+
+- ## Receiver thread for receiving complete request (completed)
+
+- ## Worker thread pool for processing request parallely (completed)
+
+- ## Static file serving (not started)
+
+- ## Replace JSONParser with SIMDjson (not started)
+
+- ## CORS configuration (not started)
+
+- ## Optimizing code a lot (working)
--- include/httpserver.h
+++ include/httpserver.h
@@ -5,9 +5,9 @@
 #include <vector>
 #include <unordered_map>
 #include <queue>
-#include <future>
-// #include <condition_variable>
-// #include <thread>
+#include <thread>
+#include <memory>
+#include <condition_variable>
 
 #include "request.h"
 #include "response.h"
@@ -96,23 +96,21 @@ class HttpServer {
   };
 
   static PathTree registeredPaths;
-  // std::queue<RequestPackage> requestQueue;
-  // std::mutex queueMutex;
-  // std::condition_variable queueCond;
+  std::queue<RequestPackage> requestQueue;
+  std::mutex queueMutex;
+  std::condition_variable queueCond;
 
   HANDLE iocp;   ///< Handle to the IO Completion Port.
-  // std::vector<std::thread> workerThreads;  ///< Worker threads for asynchronous IO processing.
 
-  // Thread-local storage for per-socket buffers.
+  // storage for per-socket buffers.
   static std::unordered_map<SOCKET, SocketBuffer> socketBuffers;
 
   static std::unordered_map<std::string, Route> allowedRoutes;  ///< Map storing allowed routes and their handlers.
   static std::vector<std::function<void(Request&, Response&, long long&)>> globalMiddlewares;  ///< Global middleware functions.
-  static std::vector<std::future<void>> requestFutures;
 
 
   static const int BUFFER_SIZE = 10240;  ///< Buffer size for socket communications.
-  // unsigned int MAX_THREADS = 1;  ///< Maximum number of worker threads.
+  unsigned int MAX_THREADS = 1;  ///< Maximum number of worker threads.
   size_t MAX_HEADER_SIZE = 8192;  ///< Maximum allowed header size.
 
   /**
@@ -174,15 +172,16 @@ class HttpServer {
    */
   static Request parseHttpRequest(const std::string &requestuest, const SOCKET &clientSocket);
 
-  static void processRequest(const std::string httpRequest, const SOCKET clientSocket);
+  // static void processRequest(const std::string httpRequest, const SOCKET clientSocket);
 
   /**
    * @brief Function executed by worker threads to handle IO completion events.
    */
-  // void workerThread();
+  void workerThreadFunction();
 
-  void receiverThread();
-
+  void receiverThreadFunction();
+  
+  void serverListen();
 public:
   /**
    * @brief Default constructor.
@@ -208,14 +207,13 @@ public:
    *
    * @param threads The number of threads.
    */
-  // inline void setThreads(unsigned int threads) { MAX_THREADS = threads; }
+  void setThreads(unsigned int threads);
 
   /**
    * @brief Starts listening for incoming connections on the provided server socket.
    *
    * @param serverSocket The server socket.
    */
-  void serverListen();
   
   /**
    * @brief Initializes the server socket and starts the IO completion port.
@@ -298,7 +296,6 @@ public:
    * Cleans up all resources, closes sockets, and clears data structures.
    */
   ~HttpServer() {
-    // workerThreads.clear();
     closesocket(serverSocket);
     for(const std::pair<const SOCKET, SocketBuffer> &it : socketBuffers)
       closesocket(it.first);
