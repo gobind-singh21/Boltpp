7c04696443594c3c4473ad6f29cd9a4556c710e1
gobind-singh21 <sgobind577@gmail.com>
2025-11-12 23:28:04 +0530

    added include guard and new error type

--- include/errors.h
+++ include/errors.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdexcept>
 
 class json_type_error : public std::runtime_error {
@@ -6,6 +8,11 @@ public:
 };
 
 class json_parse_error : public std::runtime_error {
+public:
+  using std::runtime_error::runtime_error;
+};
+
+class file_not_found : public std::runtime_error {
 public:
   using std::runtime_error::runtime_error;
 };
